345ad395d13865b2e15435750f91164bbe973c5a
Felix <felix@DESKTOP-F0L63C2.localdomain>
2023-10-03 15:32:36 +0300

    completd assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,7 +5,7 @@ const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
 describe('index.js', function () {
   describe('companyName', function () {
-    it('is set as Scuber', function () {
+    it('is set as Scuber', function() {
       expect(companyName).to.equal('Scuber');
     });
 
@@ -31,6 +31,6 @@ describe('index.js', function () {
 
     it('is defined using let', function () {
       expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
-    });
+    }); 
   });
 });
